abcfd462092b529df4ff86b0812f4df395071a51
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2024-03-15 18:26:39 -0300

    mudança de record para class em ProductDTO

--- GeekMarket.Shared/DTOs/Product/ProductDTO.cs
+++ GeekMarket.Shared/DTOs/Product/ProductDTO.cs
@@ -5,19 +5,19 @@ namespace GeekMarket.Shared.DTOs.Product
 {
     public record ProductDTO(Guid id, string Name, string Description, decimal Price, string Base64Image, int Quantity, bool Featured, DateTime LastUpdate);
 
-    public record ProductEditDTO
+    public class ProductEditDTO
     {
         [Required]
-        public string Name { get; init; } = null!;
+        public string Name { get; set; } = null!;
         [Required]
-        public string Description { get; init; } = null!;
+        public string Description { get; set; } = null!;
         [Required]
-        public decimal Price { get; init; }
+        public decimal Price { get; set; }
         [DisplayName("Imagem do produto"), Required]
-        public string Base64Image { get; init; } = null!;
+        public string Base64Image { get; set; } = null!;
         [Required]
-        public int Quantity { get; init; }
-        public bool Featured { get; init; }
+        public int Quantity { get; set; }
+        public bool Featured { get; set; }
 
         public ProductEditDTO(string name, string description, decimal price, string base64Image, int quantity, bool featured)
         {
@@ -27,6 +27,10 @@ namespace GeekMarket.Shared.DTOs.Product
             Base64Image = base64Image;
             Quantity = quantity;
             Featured = featured;
+        }
+        public ProductEditDTO()
+        {
+
         }
     }
 
