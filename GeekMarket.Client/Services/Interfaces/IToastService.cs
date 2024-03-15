404f8155b64260d73e61077a76a83ac630997ad0
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2024-03-15 18:27:57 -0300

    Criação da interface e implementação do serviço de Toasts.

---
+++ GeekMarket.Client/Services/Interfaces/IToastService.cs
@@ -0,0 +1,15 @@
+﻿using GeekMarket.Client.Components.Toast;
+
+namespace GeekMarket.Client.Services.Interfaces
+{
+    public interface IToastService
+    {
+        IReadOnlyList<ToastItem> Toasts { get; }
+
+        event EventHandler OnToastListChanged;
+
+        void AddToast(ToastItem toast);
+
+        void RemoveToast(ToastItem toast);
+    }
+}
