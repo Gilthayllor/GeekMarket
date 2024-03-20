ff4e38392e187b45c86cf499a9e6a16808972b29
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2024-03-19 22:01:37 -0300

    Adição do MudBlazor para a base da interface

--- GeekMarket.Client/Program.cs
+++ GeekMarket.Client/Program.cs
@@ -3,6 +3,7 @@ using GeekMarket.Client.Services.Implementations;
 using GeekMarket.Client.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<IToastService, ToastService>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
