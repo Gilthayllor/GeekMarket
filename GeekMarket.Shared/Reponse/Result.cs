98c19504776de425eb60df97b890830cccbeb224
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2024-03-27 07:12:09 -0300

    melhoria na classe Result

--- GeekMarket.Shared/Reponse/Result.cs
+++ GeekMarket.Shared/Reponse/Result.cs
@@ -4,7 +4,7 @@
     {
         public bool IsSuccess { get; init; }
         public string? Error { get; init; }
-        
+
         public static Result Success() => new Result { IsSuccess = true };
         public static Result Failure(string error) => new Result { IsSuccess = false, Error = error };
     }
@@ -12,7 +12,8 @@
     public class Result<T> : Result
     {
         public T? Data { get; init; }
-        
-        public static Result<T> Success<T>(T data) => new Result<T> { IsSuccess = true, Data = data };
+
+        public static Result<T> Success(T data) => new Result<T> { IsSuccess = true, Data = data };
+        public static new Result<T> Failure(string error) => new Result<T> { IsSuccess = false, Error = error };
     }
 }
